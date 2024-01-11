977252f91888ba088a6d8c65f4afb2dc38708a0b
Alexander Nitiola <cre8tor.alexander@gmail.com>
2024-01-11 12:03:20 +0100

    reading and writing to a storage file

--- src/messages/repository/messages.repository.ts
+++ src/messages/repository/messages.repository.ts
@@ -1,17 +1,30 @@
 import { readFile, writeFile } from 'fs/promises';
 
 export class MessageRepository {
+  storagePath: string = 'src/messages/repository/messages.json';
+
   async findOne(id: string): Promise<JSON> {
-    const contents = await readFile('messages.json', 'utf8');
+    const contents = await readFile(this.storagePath, 'utf8');
     const messages = JSON.parse(contents);
 
     return messages[id];
   }
 
   async findAll(): Promise<JSON> {
-    const contents = await readFile('messages.json', 'utf8');
-    return JSON.parse(contents);
+    const contents = await readFile(this.storagePath, 'utf8');
+    const messages = JSON.parse(contents);
+
+    return messages;
   }
 
-  async create(message: string) {}
+  async create(content: string) {
+    const contents = await readFile(this.storagePath, 'utf8');
+    const messages = JSON.parse(contents);
+
+    const id = Math.floor(Math.random() * 999);
+
+    messages[id] = { id, content };
+
+    await writeFile(this.storagePath, JSON.stringify(messages));
+  }
 }
