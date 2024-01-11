774fbd4acd1212a841c137ecad74fab90a5c5dc8
Alexander Nitiola <cre8tor.alexander@gmail.com>
2024-01-11 12:13:50 +0100

    implementing a service

---
+++ src/messages/service/messages.service.ts
@@ -0,0 +1,21 @@
+import { MessageRepository } from '../repository/messages.repository';
+
+export class MessagesService {
+  repository: MessageRepository;
+
+  constructor() {
+    this.repository = new MessageRepository();
+  }
+
+  async findOne(id: string): Promise<JSON> {
+    return await this.repository.findOne(id);
+  }
+
+  async findAll(): Promise<JSON> {
+    return await this.repository.findAll();
+  }
+
+  async create(content: string) {
+    await this.repository.create(content);
+  }
+}
